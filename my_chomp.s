69f6649ef0f88bffb6e755d3ff04462f1204a0c1
Benoît Dejean <bdejean@gmail.com>
2016-02-28 01:30:07 +0100

    Don't use %r15, just push/pop rdi.

--- my_chomp.s
+++ my_chomp.s
@@ -3,18 +3,17 @@
 
 .type my_chomp, @function
 my_chomp:
-	pushq %r15
-	movq %rdi, %r15
+	pushq %rdi
 	callq my_strlen
+	popq %rdi
 	# cmpq $0, %rax
 	# testq is 1-byte shorter
 	testq %rax, %rax
 	je .L_out
-	leaq -1(%r15, %rax), %rax
+	leaq -1(%rdi, %rax), %rax
 	cmpb $'\n', (%rax)
 	jne .L_out
 	movb $0, (%rax)
 	.L_out:
-	popq %r15
 	ret
 
