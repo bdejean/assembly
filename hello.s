66004a0b5642ad474561e1fa038a5daa01c93d65
Benoît Dejean <bdejean@gmail.com>
2018-02-22 20:24:05 +0100

    Use // for comments

--- hello.s
+++ hello.s
@@ -10,21 +10,21 @@ hello_addr:
 .align 4
 .global _start
 _start:
-	mov r0, $1 #stdout
+	mov r0, $1 //stdout
 	# ldr r1, hello_addr
 	ldr r1, =hello
 	mov r2, $hello_len
 	# ldr r2, =hello_len
-	mov r7, $4 #syscall
+	mov r7, $4 //syscall
 	swi $0
 
-# likewise, but calling strlen instead of hello_len
+// likewise, but calling strlen instead of hello_len
 	push {lr}
 	ldr r0, =hello
 	bl my_strlen
 	pop {lr}
 
-	mov r2, r0
+	mov r2, r0 // r0 is the result of strlen
 	mov r0, $1
 	ldr r1, =hello
 	mov r7, $4
