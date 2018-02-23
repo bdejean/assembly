625d8225f2ba29d7c996a1823fba5e7a137df3ed
Benoît Dejean <bdejean@gmail.com>
2018-02-23 20:45:43 +0100

    Use a string allocated on stack instead of global/.data variable.

--- my_strfuncs.s
+++ my_strfuncs.s
@@ -1,8 +1,4 @@
 .section .text
-.align
-
-eol:
-	.asciz "\n"
 
 .align
 .global my_print
@@ -24,8 +20,24 @@ my_print:
 my_puts:
 	push {lr}
 	bl my_print
-	ldr r0, =eol
+
+	sub sp, sp, #8
+/*
+	mov r1, #'\n'
+	strb r1, [sp]
+	mov r1, #'\0' // BUG?: loads '0' instead of '\0'
+	mov r1, #0
+	strb r1, [sp, #1]
+*/
+	mov r1, #'\n'
+	str r1, [sp] // or strh
+
+	mov r0, sp
+
 	bl my_print
+
+	add sp, sp, #8
+
 	pop {lr}
 	bx lr
 
